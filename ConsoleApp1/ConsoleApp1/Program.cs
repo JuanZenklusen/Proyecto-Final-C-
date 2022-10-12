4968f857d91b6e63b537165aad431ee823bec704
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2022-10-12 20:52:45 -0300

    Conexion al servidor

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        SqlConnectionStringBuilder conecctionbuilder = new();
+        conecctionbuilder.DataSource = "DESKTOP-97998SI";
+        conecctionbuilder.InitialCatalog = "SistemaGestion";
+        conecctionbuilder.IntegratedSecurity = true;
+        var cs = conecctionbuilder.ConnectionString;
+
+     
+    }
+}
