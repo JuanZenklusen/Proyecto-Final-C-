b3c03f797408fcc184874631e7bcbf7b03a563cf
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2022-10-22 16:45:11 -0300

    commit

---
+++ CoderAPIEjemplo/CoderAPIEjemplo/Controllers/AlumnosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoderAPIEjemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnosController : ControllerBase
+    {
+        public AlumnosController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult ObtenerAlumnos()
+        {
+            return Ok(true);
+        }
+    }
+}
---
+++ CoderAPIEjemplo/CoderAPIEjemplo/Models/Alumno.cs
@@ -0,0 +1,45 @@
+﻿namespace CoderAPIEjemplo.Models
+{
+    public class Alumno
+    {
+        #region Staments
+        private int _id;
+        private string _name;
+        private string _surname;
+        private string _email;
+        #endregion
+
+
+        #region Properties
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Surname { get { return _surname; } set { _surname = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+        #endregion
+
+
+        #region Builders
+        public Alumno()
+        {
+
+        }
+
+        public Alumno(int id, string name, string surname, string email)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Email = email;
+        }
+        #endregion
+
+
+        #region Methods
+        public List<Alumno> TraerAlumnos()
+        {
+            return new List<Alumno>();
+        }
+        #endregion
+    }
+
+}
